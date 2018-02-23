2fae98d0e9b10dcada755f6d6c0f51cdb9e4bfb1
Cristhiano Roberto Carvalho Pinto <crpinto1@stefanini.com>
2018-02-23 13:01:15 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
     BrowserModule
