98bad97642de7716a816e4490e2cb5c88bd2933c
Cristhiano Roberto Carvalho Pinto <crpinto1@stefanini.com>
2018-02-26 08:06:37 -0300

    Corrigido campo dataAquisicao

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -4,8 +4,8 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ItemService {
 
-  //url_api: string = 'http://localhost:8080/item';
-  url_api: string = 'https://patrimonio-api.herokuapp.com/item'
+  url_api: string = 'http://localhost:8080/item';
+  //url_api: string = 'https://patrimonio-api.herokuapp.com/item'
   //url_api: string = 'https://8080-dot-3005825-dot-devshell.appspot.com/item';
 
   constructor(private http: HttpClient) { }
